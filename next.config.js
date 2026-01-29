a9456a08a22ea0f0fdfff6e35dbdb3bf14514c2f
Yueyibai <143266428+yuyue1015@users.noreply.github.com>
2026-01-29 16:14:59 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  // Cloudflare Pages 不需要 basePath 或 assetPrefix，保持为空即可
+  images: { unoptimized: true }
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
