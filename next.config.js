646f41b1fb0799b5f2b9d791012dce2124d46190
Yueyibai <143266428+yuyue1015@users.noreply.github.com>
2026-01-29 15:41:27 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 开启静态导出，Cloudflare Pages 需要这个
   output: 'export',
-  
-  // 必须关闭图片优化，因为 Cloudflare Pages 没有 Node.js 服务器来压缩图片
   images: { unoptimized: true },
+  // Cloudflare Pages 不需要 basePath 或 assetPrefix，保持为空即可
 }
 
 module.exports = nextConfig;
